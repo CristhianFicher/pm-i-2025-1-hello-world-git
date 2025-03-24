e40c253249ae7cda42f40cdb9f3c5fd44d962a72
Cristhian Qughi <cristhianquoghi@hotmail.com>
2025-03-24 20:56:52 -0300

    aafollow

--- hello-world.js
+++ hello-world.js
@@ -1,5 +1,6 @@
 const nameInput = document.getElementById('name');
 const messageP = document.getElementById('message');
+const clearButton = document.getElementById('clear-button');
 
 nameInput.onkeyup = (event) => {
     if (nameInput.value === '') {
@@ -7,6 +8,25 @@ nameInput.onkeyup = (event) => {
     }
 }
 
+nameInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter' && nameInput.value.trim() !== '') {
+        helloWorld();
+    }
+});
+
+nameInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        clearFields();
+    }
+});
+
 const helloWorld = () => {
     messageP.innerText = `Hello World, ${nameInput.value}!`;
 }
+
+const clearFields = () => {
+    nameInput.value = '';
+    messageP.innerText = '';
+}
+
+clearButton.addEventListener('click', clearFields);
